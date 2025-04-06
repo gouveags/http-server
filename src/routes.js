1d7b7ed2e0ef3c0334b99f0eb9b9dded2e600d82
Gabriel Gouvea <gabriel.silva@ip.tv>
2025-04-06 03:09:00 -0300

    Add files routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 const { HTTP_METHODS } = require("./constants");
-const { stringHandler, headerHandler } = require("./functions");
+const {
+	stringHandler,
+	headerHandler,
+	fileReaderHandler,
+	fileWriterHandler,
+} = require("./functions");
 
 module.exports = [
 	{
@@ -10,6 +15,16 @@ module.exports = [
 		path: "/echo/:string",
 		handler: stringHandler,
 		method: HTTP_METHODS.GET,
+	},
+	{
+		path: "/files/:string",
+		method: HTTP_METHODS.GET,
+		handler: fileReaderHandler,
+	},
+	{
+		path: "/files/:string",
+		method: HTTP_METHODS.POST,
+		handler: fileWriterHandler,
 	},
 	{
 		path: "/user-agent",
