73ca33f8629bcd177629ed55a3816c35c5d97681
Gabriel Gouvea <gabriel.silva@ip.tv>
2025-04-06 03:10:37 -0300

    Improve and expand testing to cover new functionalities

--- src/specs/main.spec.js
+++ src/specs/main.spec.js
@@ -1,7 +1,20 @@
+const fs = require("node:fs");
 const net = require("node:net");
-const test = require("node:test");
-const assert = require("node:assert");
 const server = require("../main");
+const test = require("node:test");
+const path = require("node:path");
+const assert = require("node:assert");
+const { exec } = require("node:child_process");
+const { ensureFileExistsSync } = require("../utils");
+
+global.it = test.it;
+global.after = test.after;
+global.before = test.before;
+global.describe = test.describe;
+global.afterEach = test.afterEach;
+global.beforeEach = test.beforeEach;
+
+const fsPromises = fs.promises;
 
 const PORT = 4221;
 const HOST = "localhost";
@@ -27,62 +40,114 @@ const sendRawHttpRequest = (rawRequest) => {
 	});
 };
 
-test("GET / should respond with 200 OK", async (t) => {
-	const rawRequest = `GET / HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, /^HTTP\/1.1 200 OK/);
-});
+describe("HTTP Server Tests", () => {
+	afterEach(async () => {
+		const dirDefPath = path.resolve(__dirname, "../tmp");
 
-test("GET /echo/:string should echo the string", async (t) => {
-	const testString = "hello";
-	const rawRequest = `GET /echo/${testString} HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, new RegExp(`^HTTP/1.1 200 OK[\\s\\S]*${testString}$`));
-});
+		if (fs.existsSync(dirDefPath)) {
+			await fsPromises.rm(dirDefPath, { recursive: true }, (err) => {
+				console.log(err);
+				return;
+			});
+		}
+	});
 
-test("GET /user-agent should return User-Agent header", async (t) => {
-	const userAgent = "TestAgent";
-	const rawRequest = [
-		"GET /user-agent HTTP/1.1",
-		`Host: ${HOST}`,
-		"Accept: */*",
-		`User-Agent: ${userAgent}`,
-		"",
-		"",
-	].join("\r\n");
+	it("GET / should respond with 200 OK", async () => {
+		const rawRequest = `GET / HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 200 OK/);
+	});
 
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, new RegExp(`^HTTP/1.1 200 OK[\\s\\S]*${userAgent}$`));
-});
+	it("GET /echo/:string should echo the string", async () => {
+		const testString = "hello";
+		const rawRequest = `GET /echo/${testString} HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(
+			response,
+			new RegExp(`^HTTP/1.1 200 OK[\\s\\S]*${testString}$`),
+		);
+	});
 
-test("GET /not-found should return 404 Not Found", async (t) => {
-	const rawRequest = `GET /not-found HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, /^HTTP\/1.1 404 Not Found/);
-});
+	it("GET /user-agent should return User-Agent header", async () => {
+		const userAgent = "TestAgent";
+		const rawRequest = [
+			"GET /user-agent HTTP/1.1",
+			`Host: ${HOST}`,
+			"Accept: */*",
+			`User-Agent: ${userAgent}`,
+			"",
+			"",
+		].join("\r\n");
 
-test("Invalid HTTP method should return 500 Internal Server Error", async (t) => {
-	const rawRequest = `INVALID / HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, /^HTTP\/1.1 500 Internal Server Error/);
-});
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(
+			response,
+			new RegExp(`^HTTP/1.1 200 OK[\\s\\S]*${userAgent}$`),
+		);
+	});
 
-test("Invalid HTTP version should return 500 Internal Server Error", async (t) => {
-	const rawRequest = `GET / HTTP/1.0\r\nHost: ${HOST}\r\n\r\n`;
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, /^HTTP\/1.1 500 Internal Server Error/);
-});
+	it("GET /not-found should return 404 Not Found", async () => {
+		const rawRequest = `GET /not-found HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 404 Not Found/);
+	});
 
-test("Malformed User-Agent header should return 400 Bad Request", async (t) => {
-	const rawRequest = [
-		"GET /user-agent HTTP/1.1",
-		`Host: ${HOST}`,
-		"Accept: */*",
-		"UserAgent Missing-colon", // Malformed header
-		"",
-		"",
-	].join("\r\n");
+	it("Invalid HTTP method should return 500 Internal Server Error", async () => {
+		const rawRequest = `INVALID / HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 500 Internal Server Error/);
+	});
 
-	const response = await sendRawHttpRequest(rawRequest);
-	assert.match(response, /^HTTP\/1.1 400 Bad Request/);
+	it("Invalid HTTP version should return 500 Internal Server Error", async () => {
+		const rawRequest = `GET / HTTP/1.0\r\nHost: ${HOST}\r\n\r\n`;
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 500 Internal Server Error/);
+	});
+
+	it("Malformed User-Agent header should return 400 Bad Request", async () => {
+		const rawRequest = [
+			"GET /user-agent HTTP/1.1",
+			`Host: ${HOST}`,
+			"Accept: */*",
+			"UserAgent Missing-colon",
+			"",
+			"",
+		].join("\r\n");
+
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 400 Bad Request/);
+	});
+
+	it("Should return valid file's content", async () => {
+		const fileName = "foo";
+		const filepath = `../tmp/${fileName}`;
+		const fileContent = "Hello World!";
+
+		await ensureFileExistsSync(filepath, fileContent);
+
+		const rawRequest = `GET /files/${fileName} HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(
+			response,
+			new RegExp(`^HTTP/1.1 200 OK[\\s\\S]*${fileContent}$`),
+		);
+	});
+
+	it("Should return 404 Not Found for a file that doesn't exist", async () => {
+		const fileName = "foo";
+		const rawRequest = `GET /files/${fileName} HTTP/1.1\r\nHost: ${HOST}\r\n\r\n`;
+
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 404 Not Found/);
+	});
+
+	it("GET / should respond with 201 Created", async () => {
+		const body = "12345";
+		const fileName = "file_123";
+		const rawRequest = `POST /files/${fileName} HTTP/1.1\r\nHost: ${HOST}\r\n\r\n${body}`;
+
+		const response = await sendRawHttpRequest(rawRequest);
+		assert.match(response, /^HTTP\/1.1 201 Created/);
+	});
 });
