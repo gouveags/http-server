b0242c45643957ca9ef26943f9bb93a096d8c336
Gabriel Gouvea <gabriel.silva@ip.tv>
2025-04-06 03:09:49 -0300

    Create fileReaderHandler and fileWriterHandler functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,7 +1,42 @@
-const { HttpError } = require("../utils");
+const fs = require("node:fs");
+const path = require("node:path");
 const { HTTP_STATUS_CODES } = require("../constants");
+const { HttpError, ensureFileExistsSync } = require("../utils");
+
+const BASE_DIR = path.resolve(__dirname, "../tmp");
 
 const stringHandler = ({ query }) => query;
+
+const fileReaderHandler = ({ query, body }) => {
+	const requestedPath = path.resolve(BASE_DIR, query);
+
+	if (!requestedPath.startsWith(BASE_DIR)) {
+		throw HttpError("Error", HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+	}
+
+	try {
+		const fileContent = fs.readFileSync(requestedPath, "utf8");
+		return fileContent;
+	} catch (err) {
+		throw HttpError("File Not Found", HTTP_STATUS_CODES.NOT_FOUND);
+	}
+};
+
+const fileWriterHandler = async ({ query, body }) => {
+	const requestedPath = path.resolve(BASE_DIR, query);
+
+	if (!requestedPath.startsWith(BASE_DIR)) {
+		throw HttpError("Error", HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR);
+	}
+
+	try {
+		console.log({ requestedPath, body });
+		const fileContent = await ensureFileExistsSync(requestedPath, body);
+		return { status: HTTP_STATUS_CODES.CREATED };
+	} catch (err) {
+		throw HttpError("File Not Found", HTTP_STATUS_CODES.NOT_FOUND);
+	}
+};
 
 const headerHandler = ({ headers }) => {
 	const [host, accept, userAgent] = headers.split("\r\n");
@@ -14,4 +49,9 @@ const headerHandler = ({ headers }) => {
 	throw HttpError("Header Malformed", HTTP_STATUS_CODES.BAD_REQUEST);
 };
 
-module.exports = { stringHandler, headerHandler };
+module.exports = {
+	stringHandler,
+	headerHandler,
+	fileReaderHandler,
+	fileWriterHandler,
+};
